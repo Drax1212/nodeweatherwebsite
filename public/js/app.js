4c0b23ae90de37fd02c323fc63ebbd7162d36ee6
Suyog Tiwari <stonesjohn23@gmail.com>
2020-07-06 04:21:41 +0545

    Remove console call

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,20 @@
-console.log("Client side JS");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  messageOne.innerHTML = "Loading...</span>";
 
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
-form.addEventListener('submit', (e) => {
-
-    e.preventDefault();
-    messageOne.innerHTML = 'Loading...</span>'
-
-
-    messageTwo.textContent = ''
-    axios
-        .get(`http://localhost:3000/weather?address=${input.value}`)
-        .then((res) => {
-            if (res.data.error) {
-                messageOne.textContent = res.data.error
-                return
-            }
-            messageOne.textContent = res.data.location
-            messageTwo.textContent = res.data.forecast;
-        });
-
-})
+  messageTwo.textContent = "";
+  axios
+    .get(`http://localhost:3000/weather?address=${input.value}`)
+    .then((res) => {
+      if (res.data.error) {
+        messageOne.textContent = res.data.error;
+        return;
+      }
+      messageOne.textContent = res.data.location;
+      messageTwo.textContent = res.data.forecast;
+    });
+});
